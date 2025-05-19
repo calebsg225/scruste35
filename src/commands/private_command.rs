b54e2262923f7afd4c5f6186a6313478f14dd51e
calebsg225 <calebsg2253284@gmail.com>
2025-05-19 15:01:53 -0400

    Implement PrivateCommand

--- src/commands/private_command.rs
+++ src/commands/private_command.rs
@@ -3,10 +3,17 @@
 use crate::bitbin::BitRead;
 
 /// Table 13 - private_command()
-pub struct PrivateCommand {}
+pub struct PrivateCommand {
+    identifier: u32,
+    private_bytes: Vec<u8>,
+}
 
 impl PrivateCommand {
     pub fn from(bytes: &[u8]) -> Self {
-        Self {}
+        let mut bread = BitRead::from(bytes);
+        Self {
+            identifier: bread.as_int(32) as u32,
+            private_bytes: bread.as_bytes(bread.get_idx()),
+        }
     }
 }
