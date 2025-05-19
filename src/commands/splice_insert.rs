c24de2c1f0ec30039de4c76a56ae7ecc42ce0c82
calebsg225 <calebsg2253284@gmail.com>
2025-05-19 14:42:09 -0400

    Implement SpliceInsert

--- src/commands/splice_insert.rs
+++ src/commands/splice_insert.rs
@@ -1,12 +1,16 @@
 //! src/commands/splice_insert.rs
 
 use crate::bitbin::BitRead;
+use crate::commands::utils::SpliceEvent;
 
 /// Table 10 - splice_insert()
-pub struct SpliceInsert {}
+pub struct SpliceInsert {
+    splice_event: SpliceEvent,
+}
 
 impl SpliceInsert {
     pub fn from(bytes: &[u8]) -> Self {
-        Self {}
+        let (splice_event, _) = SpliceEvent::from_insert(BitRead::from(bytes));
+        Self { splice_event }
     }
 }
