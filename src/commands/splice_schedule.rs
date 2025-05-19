c30e29511bc0bdce17911acf6fd56889dc05e704
calebsg225 <calebsg2253284@gmail.com>
2025-05-18 21:39:29 -0400

    Begin decoding for the `splice_schedule()` splice command

--- src/commands/splice_schedule.rs
+++ src/commands/splice_schedule.rs
@@ -1,9 +1,31 @@
 //! src/commands/splice_schedule.rs
 
-pub struct SpliceSchedule {}
+use crate::bitbin::BitRead;
+use crate::commands::utils::SpliceEvent;
+
+/// Table 9 - splice_schedule()
+pub struct SpliceSchedule {
+    splice_count: u8,
+    splice_events: Vec<SpliceEvent>,
+}
 
 impl SpliceSchedule {
-    pub fn from(bread: &mut BitRead) -> Self {
-        Self {}
+    pub fn from(bytes: &[u8]) -> Self {
+        let mut bread = BitRead::from(bytes);
+        let splice_count = bread.as_int(8) as u8;
+        let mut splice_events: Vec<SpliceEvent> = Vec::new();
+
+        for _ in 0..splice_count {
+            let splice_event: SpliceEvent;
+            // NOTE: currently passing same instance of `bread` to and from each `SpliceEvent`.
+            // TODO: find a better way? keep track of the bits decoded instead?
+            (splice_event, bread) = SpliceEvent::from_schedule(bread);
+            splice_events.push(splice_event);
+        }
+
+        Self {
+            splice_count,
+            splice_events,
+        }
     }
 }
